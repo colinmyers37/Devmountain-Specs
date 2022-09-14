de9a3f3fe35f2182b1d2a76117fe0eda1ecfa9e8
colinmyers37 <colinmyers37@gmail.com>
2022-09-14 00:12:41 -0500

    finished day 2

---
+++ week2/movie-watchlist/src/components/MovieScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MovieScreen({movieList, page, setPage, list}) {
+  const movieDisplay = movieList.map((movie, index) => {
+    return <h2>{movie.original_title}</h2>;
+  });
+  return (
+    <div className="page">
+      <h1>Colin's Movie Theatre</h1>
+      <h3>Add a movie to your watchlist</h3>
+<div className='movie-container'>{movieDisplay}</div>
+    </div>
+
+  );
+}
+
+export default MovieScreen;
