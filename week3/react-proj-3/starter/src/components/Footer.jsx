8511bb3b49237372a94c0d789165563623e2e67f
colinmyers37 <colinmyers37@gmail.com>
2022-09-22 21:30:42 -0500

    finished day 4

--- week3/react-proj-3/starter/src/components/Footer.jsx
+++ week3/react-proj-3/starter/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
 import styles from "./Header.module.css";
+import { ImFacebook, ImInstagram, ImTwitter, ImGithub } from "react-icons/im";
 
 const Footer = () => {
   return (
@@ -8,36 +8,21 @@ const Footer = () => {
       <div className={styles.social_container}>
         <h5>Hit us up on our socials!</h5>
         <div className={styles.social_icons}>
-          <a
-            href="https://www.devmountain.com"
-            target="_blank"
-            className={styles.circle}
-          >
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImFacebook size="1.5em" color="#3f3f3f" />
           </a>
-          <a
-            href="https://www.devmountain.com"
-            target="_blank"
-            className={styles.circle}
-          >
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImInstagram size="1.5em" color="#3f3f3f" />
           </a>
-          <a
-            href="https://www.devmountain.com"
-            target="_blank"
-            className={styles.circle}
-          >
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImTwitter size="1.5em" color="#3f3f3f" />
           </a>
-          <a
-            href="https://www.devmountain.com"
-            target="_blank"
-            className={styles.circle}
-          >
+          <a href="https://www.devmountain.com" target="_blank" className={styles.circle}>
             <ImGithub size="1.5em" color="#3f3f3f" />
           </a>
         </div>
       </div>
+      {/* <div> */}
       <svg
         width="110"
         height="110"
@@ -54,6 +39,7 @@ const Footer = () => {
           stroke-linejoin="round"
         />
       </svg>
+      {/* </div> */}
     </footer>
   );
 };
